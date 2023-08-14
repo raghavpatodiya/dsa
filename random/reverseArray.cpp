4a0663d9860c7b6feb20e6a33adbb9828fa8cc62
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-08-14 21:08:08 +0530

    commit

--- random/reverseArray.cpp
+++ random/reverseArray.cpp
@@ -13,6 +13,7 @@ int main(){
     int arr[]= {1, 2, 3, 4, 5, 6, 7, 8};
     int size = sizeof(arr)/sizeof(arr[0]);
     reverseArray(arr, size);
+    // print array
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
