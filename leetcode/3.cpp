da8a3e256bbda51aabf57185af0b61f58365fa6c
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2023-08-16 08:47:41 +0530

    leetcode

---
+++ leetcode/3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        
+        int maxLength = 0;
+        int start = 0; // Starting index of the current substring     
+        // Create a map to track the last occurrence index of each character
+        unordered_map<char, int> lastIndex;   // key, value   
+        for (int end = 0; end < n; ++end) {
+            if (lastIndex.find(s[end]) != lastIndex.end()) {
+                // If the current character is already in the substring, update the start
+                // to the next index after the last occurrence of this character.
+                start = max(start, lastIndex[s[end]] + 1);
+            }          
+            // Update the last occurrence index of the current character
+            lastIndex[s[end]] = end;           
+            // Calculate the length of the current substring and update maxLength
+            maxLength = max(maxLength, end - start + 1);
+        }
+        
+        return maxLength;
+    }
+};
---
+++ leetcode/5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        //start and end
+        // making a matrix like structure helps 
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        string ans;
+        int maxLength = 0;
+        for(int diff=0; diff<n;diff++){
+            for(int i=0, j=i+diff; j<n; i++, j++){
+                if(i==j){//diagonal
+                    dp[i][j]=1;
+                }
+                else if(diff==1){
+                    dp[i][j]=(s[i]==s[j])?2:0;
+                }
+                else{
+                    if(s[i]==s[j] && dp[i+1][j-1]>0){
+                        dp[i][j] = dp[i+1][j-1]+2; 
+                    }
+                }
+                if(dp[i][j]){
+                    if(j-i+1>maxLength){
+                        maxLength=j-i+1;
+                        ans = s.substr(i, maxLength);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
